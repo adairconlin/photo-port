f6ca53a504f0ce5488afc7af424f85e6536aa1f0
adairconlin <adair.conlin@gmail.com>
2022-02-18 15:25:24 -0500

    Add and pass tests for About component

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
+afterEach(cleanup); // after each test, there will be no leftover memory data
+
+describe("About component", () => {
+    it("renders", () => {
+        render(<About />);
+    });
+
+    it("matches snapshot DOM node structure", () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
