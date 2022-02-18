07948586b183ece8c1705665139a60533adc281c
adairconlin <adair.conlin@gmail.com>
2022-02-18 15:37:38 -0500

    Add Nav test and pass

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -21,13 +21,13 @@ function Nav() {
     return (
         <header>
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera">📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
-                    <li className="mx-2">
+                    <li data-testid="about" className="mx-2">
                         <a href="#about">
                             About me
                         </a>
