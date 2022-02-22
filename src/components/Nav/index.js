e0ad25b50ed24b0a5a7b2a4759c41f41d8ea366b
adairconlin <adair.conlin@gmail.com>
2022-02-22 12:45:52 -0500

    Add conditional rendering

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
         categories = [],
         setCurrentCategory,
         currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
 
     return (
@@ -17,21 +19,22 @@ function Nav(props) {
             </h2>
             <nav>
                 <ul className="flex-row">
-                    <li data-testid="about" className="mx-2">
+                    <li data-testid="about" className="mx-2" onClick={() => setContactSelected(false)}>
                         <a href="#about">
                             About me
                         </a>
                     </li>
-                    <li className={"mx-2"}>
+                    <li className={`mx-2 ${contactSelected && "navActive"}`} onClick={() => setContactSelected(true)}>
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li 
-                            className={`mx-1 ${ currentCategory.name === category.name }`} 
+                            className={`mx-1 ${ currentCategory.name === category.name && !contactSelected && `navActive` }`} 
                             key={category.name}>
                             <span 
                                 onClick={() => {
                                     setCurrentCategory(category)
+                                    setContactSelected(false);
                                 }}>
                                 {capitalizeFirstLetter(category.name)}
                             </span>
